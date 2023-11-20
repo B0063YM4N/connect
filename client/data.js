ab9c960adbb5c0daa2e753893f2b4b51f56ec33c
B0063YM4N <gogatadzebeka@gmail.com>
2023-11-21 01:43:29 +0400

    pls

---
+++ client/data.js
@@ -0,0 +1,31 @@
+const data = {
+    message: "ok",
+    data: [
+      {
+        id: 1,
+        nameGe: "რეგულარი",
+        nameEn: "Regular",
+        price: 1,
+      },
+      {
+        id: 2,
+        nameGe: "პრემიუმი",
+        nameEn: "Premium",
+        price:1,
+      },
+      {
+        id: 3,
+        nameGe: "დიზელი",
+        nameEn: "Diesel",
+        price: 1,
+      },
+      {
+        id: 4,
+        nameGe: "ევრო დიზელი",
+        nameEn: "Euro Diesel",
+        price: 1,
+      },
+    ],
+  };
+
+  export default data;
---
+++ client/src/app/api/data/route.js
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server";
+import data from "../../../../data";
+
+export const GET = async (req) => {
+    try {
+      console.log("ae");
+    } catch (error) {
+      console.log(error);
+    }
+  return new NextResponse(JSON.stringify(data), { status: 201 });
+}
+
+
+export const POST = async (req) => {
+  const { price1, price2, price3, price4 } = await req.json();
+
+  if (price1 !== undefined && price2 !== undefined && price3 !== undefined && price4 !== undefined) {
+    data.data[0].price = price1;
+    data.data[1].price = price2;
+    data.data[2].price = price3;
+    data.data[3].price = price4;
+  }
+  
+
+  return new NextResponse(JSON.stringify(data), { status: 201 });
+}
