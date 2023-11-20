f4c5097e42e82ffb379d3fb7d29595e99f205ba2
B0063YM4N <gogatadzebeka@gmail.com>
2023-11-21 01:28:54 +0400

    asdaaaaa

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -27,6 +27,7 @@ const Products = ({title}) => {
       const fetcher = (...args) => fetch(...args).then((res) => res.json());
       const { data, mutate, error, isLoading } = useSWR(`https://connect-database.vercel.app/api/data`, fetcher);
 
+      mutate()
       console.log(data);
 
     return (
